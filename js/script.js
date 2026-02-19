6de4703d60890b85b2829a7b21c30f6ce614f901
Quentin Lorenzo <quentinlorenzo04@gmail.com>
2026-02-19 15:17:56 +0100

    Suppression du préchargeur et simplification des transitions dans le CSS et le JavaScript

--- js/script.js
+++ js/script.js
@@ -28,8 +28,6 @@ $(function(){
 	});
 	
 	$(window).on('load', function(){
-		$('body').addClass('loaded');
-		
 		/*=========================================================================
 			Portfolio Grid
 		=========================================================================*/
@@ -52,20 +50,9 @@ $(function(){
 			sect = $( '#' + $this.data('section') ),
 			current_sect = $('.section.active');
 		if(sect.length == 1){
-			if( sect.hasClass('active') == false && $('body').hasClass('section-switching') == false ){
-				$('body').addClass('section-switching');
-				if( sect.index() < current_sect.index() ){
-					$('body').addClass('up');
-				}else{
-					$('body').addClass('down');
-				}
-				setTimeout(function(){
-					$('body').removeClass('section-switching up down');			
-				}, 2500);
-				setTimeout(function(){
-					current_sect.removeClass('active');
-					sect.addClass('active');
-				}, 1250);
+			if( sect.hasClass('active') == false ){
+				current_sect.removeClass('active');
+				sect.addClass('active');
 				if( sect.hasClass('border-d') ){
 					$('body').addClass('border-dark');
 				}else{
